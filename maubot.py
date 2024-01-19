4a42d6796a82ddfb9e1d6d0c48ad6ebb747a7758
brian <brian@mutualaid.info>
2024-01-19 16:01:07 -0500

    Build workflow

---
+++ maubot.py
@@ -0,0 +1,9 @@
+import os
+import yaml
+
+
+with open("maubot.yaml") as f:
+    maubot = yaml.safe_load(f)
+maubot["version"] = os.environ["BOT_VERSION"]
+with open("maubot.yml", "w") as f:
+    yaml.safe_dump(maubot, f)
